3fa99d795657ed7188cdc919c1bad5154083548f
warmab13 <alonsosalcido13@gmail.com>
2022-08-11 14:54:05 -0600

    use DTOS and change services and controllers to use DTOS and interface

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -11,35 +11,34 @@ import {
 } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { Request } from 'express';
-
+import { BookDto } from './book.dto';
+import { Book } from './book.class';
 @Controller('books')
 export class BooksController {
   constructor(private booksService: BooksService) {}
 
-  @Get()
-  findAll(@Req() request: Request) {
+  findAll(@Req() request: Request): Book[] {
+    console.log(request.query);
     return this.booksService.findAll(request.query);
   }
 
   @Get(':bookId')
-  findBook(@Param('bookId') bookId: string) {
+  findBook(@Param('bookId') bookId: string): Book {
     return this.booksService.findBook(bookId);
   }
 
   @Post()
-  createBook(@Body() body) {
-    const newBook: any = body;
+  createBook(@Body() newBook: BookDto): Book {
     return this.booksService.createBook(newBook);
   }
 
   @Delete(':bookId')
-  deleteBook(@Param('bookId') bookId: string) {
+  deleteBook(@Param('bookId') bookId: string): Book {
     return this.booksService.deleteBook(bookId);
   }
 
   @Put(':bookId')
-  updateBook(@Param('bookId') bookId: string, @Body() body) {
-    const newBook: any = body;
+  updateBook(@Param('bookId') bookId: string, @Body() newBook: BookDto) {
     return this.booksService.updateBook(bookId, newBook);
   }
 }
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,34 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpStatus, HttpException } from '@nestjs/common';
+import { BookDto } from './book.dto';
+import { Book } from './book.class';
 
 @Injectable()
 export class BooksService {
-  findAll(params): any {
-    let msg = `findAll funcionando. Parámetros:`;
-
-    if (params.order !== undefined) {
-      msg = msg + ` order: ${params.order}`;
-    }
-
-    if (params.limit !== undefined) {
-      msg = msg + ` limit: ${params.limit}`;
-    }
-
-    return msg;
+  books: Book[] = [
+    {
+      id: 1,
+      title: 'Una historia de España',
+      genre: 'Historia',
+      description:
+        'Un relato ameno, personal, a ratos irónico, pero siempre único, de nuestra accidentada historia a través de los siglos. Una obra concebida por el autor para, en palabras suyas, «divertirme, releer y disfrutar; un pretexto para mirar atrás desde los tiempos remotos hasta el presente, reflexionar un poco sobre ello y contarlo por escrito de una manera poco ortodoxa.',
+      author: 'Arturo Pérez-Reverte',
+      publisher: 'Alfaguara',
+      pages: 256,
+      image_url:
+        'https://images-na.ssl-images-amazon.com/images/I/41%2B-e981m1L._SX311_BO1,204,203,200_.jpg',
+    },
+    {
+      id: 2,
+      title: 'Historia de España contada para escépticos',
+      genre: 'Historia',
+      description:
+        'Como escribe el autor, no pretende ser veraz, justa y desapasionada, porque ninguna historia lo es. No está hecha para halagar a reyes y gobernantes, ni pretende halagar a los banqueros, ni a la Conferencia Episcopal, ni al colectivo gay.',
+      author: 'Juan Eslava Galán',
+      publisher: 'Booket',
+      pages: 592,
+      image_url:
+        'https://images-na.ssl-images-amazon.com/images/I/51IyZ5Mq8YL._SX326_BO1,204,203,200_.jpg',
+    },
+  ];
+  findAll(params): Book[] {
+    return this.books;
   }
 
-  findBook(bookId: string) {
-    return `findBook funcionando con bookId: ${bookId}`;
+  findBook(bookId: string): Book {
+    return this.books[parseInt(bookId) - 1];
   }
 
-  createBook(newBook: any) {
-    return newBook;
+  createBook(newBook: BookDto): Book {
+    let book = new Book();
+
+    book.id = 99;
+    book.author = newBook.author;
+    book.description = newBook.description;
+    book.genre = newBook.genre;
+    book.image_url = newBook.image_url;
+    book.pages = newBook.pages;
+    book.publisher = newBook.publisher;
+    book.title = newBook.title;
+
+    return book;
   }
 
-  deleteBook(bookId: string) {
-    return `deleteBook funcionando con bookId ${bookId}`;
+  deleteBook(bookId: string): Book {
+    return this.books[parseInt(bookId) - 1];
   }
 
-  updateBook(bookId: string, newBook: any) {
-    return newBook;
+  updateBook(bookId: string, newBook: BookDto): Book {
+    return this.books[parseInt(bookId) - 1];
   }
 }
